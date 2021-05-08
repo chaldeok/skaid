c04fc17fa24ba76ea040dc4292deb53ba5714bf5
Vlad Choi <vlad_choi@MacBook-Air-Vlad.local>
2021-05-08 14:54:22 +0300

    main-section spoilers, spoilers js and styles, bug fixing

--- app/js/spoilers.js
+++ app/js/spoilers.js
@@ -1,3 +1,28 @@
-setTimeout(() => {
-  console.log('js works!')
-}, 1000)
+let spoilers = document.querySelectorAll('.spoiler__title');
+let spoilersWrapper = document.querySelector('.spoilers-wrapper');
+
+for(let i = 0; i < spoilers.length; i++) {
+  spoilers[i].addEventListener('click', function(event) {
+    if(spoilersWrapper.classList.contains('spoilers-wrapper_oneOnly')) {
+      for(i = 0; i < spoilers.length; i++) {
+        if(spoilers[i] !== event.target) {
+          spoilers[i].classList.remove('spoiler__title_active');
+          spoilers[i].nextElementSibling.style.maxHeight = null;
+
+          this.parentElement.parentElement.parentElement.classList.remove('company-spoiler');
+          this.parentElement.previousElementSibling.classList.remove('article__img--active');
+        }
+      }
+    }
+    this.classList.toggle('spoiler__title_active');
+    this.parentElement.parentElement.parentElement.classList.toggle('company-spoiler');
+    this.parentElement.previousElementSibling.classList.toggle('article__img--active');
+
+    let text = this.nextElementSibling;
+    if(text.style.maxHeight) {
+      text.style.maxHeight = null;
+    } else {
+      text.style.maxHeight = text.scrollHeight + 'px';
+    }
+  })
+}
--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,37 @@
 "use strict";
 
-setTimeout(function () {
-  console.log('js works!');
-}, 1000);
+var spoilers = document.querySelectorAll('.spoiler__title');
+var spoilersWrapper = document.querySelector('.spoilers-wrapper');
+
+var _loop = function _loop(_i) {
+  spoilers[_i].addEventListener('click', function (event) {
+    if (spoilersWrapper.classList.contains('spoilers-wrapper_oneOnly')) {
+      for (_i = 0; _i < spoilers.length; _i++) {
+        if (spoilers[_i] !== event.target) {
+          spoilers[_i].classList.remove('spoiler__title_active');
+
+          spoilers[_i].nextElementSibling.style.maxHeight = null;
+          this.parentElement.parentElement.parentElement.classList.remove('company-spoiler');
+          this.parentElement.previousElementSibling.classList.remove('article__img--active');
+        }
+      }
+    }
+
+    this.classList.toggle('spoiler__title_active');
+    this.parentElement.parentElement.parentElement.classList.toggle('company-spoiler');
+    this.parentElement.previousElementSibling.classList.toggle('article__img--active');
+    var text = this.nextElementSibling;
+
+    if (text.style.maxHeight) {
+      text.style.maxHeight = null;
+    } else {
+      text.style.maxHeight = text.scrollHeight + 'px';
+    }
+  });
+
+  i = _i;
+};
+
+for (var i = 0; i < spoilers.length; i++) {
+  _loop(i);
+}
